78a6c1d56169f65d7dfc39b838503f1e95729578
Guillermo Villegas <44106664+gevillegas@users.noreply.github.com>
2019-04-15 13:35:07 -0400

    Add files via upload

---
+++ tickers.py
@@ -0,0 +1,15 @@
+from iex import Stock
+
+
+#class Tickers():
+
+
+
+def save_tickers(n):
+    """
+    fetches first n valid tickers from url and writes to tickers.txt
+    """
+
+
+if __name__ == "__main__":
+    
